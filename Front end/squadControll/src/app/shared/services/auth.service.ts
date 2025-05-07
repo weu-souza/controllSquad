0a43d996df36faa0db371fc4e58090bda275c771
weu-souza <weuller1561@gmail.com>
2025-05-07 12:40:48 -0300

    replace auth service from firebase to httpService

--- Front end/squadControll/src/app/shared/services/auth.service.ts
+++ Front end/squadControll/src/app/shared/services/auth.service.ts
@@ -1,131 +1,84 @@
 import { Injectable } from '@angular/core';
 import { User } from './user';
-import { AngularFireAuth } from '@angular/fire/compat/auth';
-import { AngularFirestore } from '@angular/fire/compat/firestore';
+import { HttpClient } from '@angular/common/http';
 import { Router } from '@angular/router';
 import { Storage } from 'src/constants/storage.cte';
 import { AppRoutes } from 'src/constants/app-routes.cte';
-
+import { environment } from 'src/environments/environment';
 
 @Injectable({
     providedIn: 'root',
 })
-export class AuthService
-{
+export class AuthService {
     userData: any;
+    private apiUrl = environment.ApiUrl; 
 
     constructor(
-        public firestore: AngularFirestore,
-        public fireAuth: AngularFireAuth,
-        public router: Router)
-    {
-        this.fireAuth.authState.subscribe(user =>
-        {
-            if (user)
-            {
-                this.userData = user;
-                localStorage.setItem(Storage.User, JSON.stringify(this.userData));
-                JSON.parse(localStorage.getItem(Storage.User)!);
-                this.router.navigate([AppRoutes.Home])
-            } 
-            else
-            {
-                localStorage.setItem(Storage.User, 'null');
-                JSON.parse(localStorage.getItem(Storage.User)!);
-            }
-        });
+        private http: HttpClient,
+        private router: Router
+    ) {
+        const user = localStorage.getItem(Storage.User);
+        if (user && user !== 'null') {
+            this.userData = JSON.parse(user);
+            this.router.navigate([AppRoutes.Home]);
+        }
     }
 
-
-    signIn(email: string, password: string)
-    {
-        return this.fireAuth
-            .signInWithEmailAndPassword(email, password)
-            .then(result =>
-            {
-                this.setUserData(result.user);
-                this.fireAuth.authState.subscribe((user) =>
-                {
-                    if (user)
-                    {
-                        this.router.navigate([AppRoutes.Home]);
-                    }
-                });
-            })
-            .catch(error =>
-            {
-                window.alert(error.message);
+    signIn(email: string, password: string) {
+        return this.http.post<any>(`${this.apiUrl}/auth/login`, { email, password })
+            .subscribe({
+                next: (response) => {
+                    this.setUserData(response.user);
+                    this.router.navigate([AppRoutes.Home]);
+                },
+                error: (error) => {
+                    window.alert(error.message);
+                }
             });
     }
 
-    signUp(email: string, password: string, name: string)
-    {
-        return this.fireAuth
-            .createUserWithEmailAndPassword(email, password)
-            .then((result:any) =>
-            {
-                // this.sendVerificationMail();
-                result.user._delegate.displayName  = name;
-                this.fireAuth.updateCurrentUser(result.user._delegate)
-                this.setUserData(result.user._delegate);
-                this.signIn(email, password);
-            })
-            .catch((error) =>
-            {
-                window.alert(error.message);
+    signUp(email: string, password: string, name: string) {
+        return this.http.post<any>(`${this.apiUrl}/auth/register`, { email, password, name })
+            .subscribe({
+                next: (response) => {
+                    this.setUserData(response.user);
+                    this.signIn(email, password);
+                },
+                error: (error) => {
+                    window.alert(error.message);
+                }
             });
     }
 
-    sendVerificationMail()
-    {
-        return this.fireAuth.currentUser
-            .then((u: any) => u.sendEmailVerification())
-            .then(() =>
-            {
-                this.router.navigate(['verify-email-address']);
+    forgotPassword(email: string) {
+        return this.http.post<any>(`${this.apiUrl}/auth/forgot-password`, { email })
+            .subscribe({
+                next: () => {
+                    window.alert('Password reset email sent, check your inbox.');
+                },
+                error: (error) => {
+                    window.alert(error.message);
+                }
             });
     }
 
-    forgotPassword(passwordResetEmail: string)
-    {
-        return this.fireAuth
-            .sendPasswordResetEmail(passwordResetEmail)
-            .then(() =>
-            {
-                window.alert('Password reset email sent, check your inbox.');
-            })
-            .catch((error) =>
-            {
-                window.alert(error);
-            });
-    }
-
-    get isLoggedIn(): boolean
-    {
+    get isLoggedIn(): boolean {
         const user = JSON.parse(localStorage.getItem(Storage.User)!);
-        return user !== null ? true : false;
+        return user !== null;
     }
 
-    setUserData(user: any)
-    {
-        const firestoreDoc = FirestoreRoutes.UserByEmail.replace(':email', user.email)
-        const userRef = this.firestore.doc(firestoreDoc);
-
+    setUserData(user: any) {
         const userData: User = {
             uid: user.uid,
             email: user.email,
             emailVerified: user.emailVerified,
         };
-        return userRef.set(userData, {
-            merge: true,
-        });
+        localStorage.setItem(Storage.User, JSON.stringify(userData));
+        return userData;
     }
 
     signOut() {
-        return this.fireAuth.signOut().then(() => {
-            console.log('Signed Out');
-            localStorage.removeItem(Storage.User);
-            this.router.navigate([AppRoutes.Login]);
-        });
+        localStorage.removeItem(Storage.User);
+        this.router.navigate([AppRoutes.Login]);
     }
 }
